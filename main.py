8ca8f6a80ae03c377f1b347083fd41a590bdf323
sami <msami.bese22seecs@seecs.edu.pk>
2023-07-04 04:52:06 +0500

    New

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Response,status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 app=FastAPI()
 
+
+class Post(BaseModel):
+    title : str
+    content : str
+    publication : bool=True
+
+
+my_post=[{"title":"Post1","content":"this is Post Number 1","id":1},{"title":"Post2","content":"this is second post","id":2}]
 @app.get("/")
 def root():
     return {"message":"Hello1234567"}
@@ -10,11 +19,34 @@ def root():
 @app.get("/posts")
 
 def get_posts():
-    return {"Post":"Hello Everyone "}
+    return {"Post":my_post}
 
 
-@app.post("/createpost")
+@app.post("/posts")
 
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"Message": "new"}
+def create_posts(new_post: Post):
+    print(new_post.dict())
+    post_dict=new_post.dict()
+    post_dict['id']=randrange(0,100000000)
+    my_post.append(post_dict)
+    return {"Message": my_post}
+
+def find_post(id):
+    for p in my_post:
+        if p["id"]==id:
+            return p
+@app.get("/posts/{id}")
+
+def get_posts(id:int):
+  print(id)
+  post1=find_post(id)
+  if not post1:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Not Found")
+  return  {"post detail":post1}
+
+
+
+
+#@app.delete("/posts/{id}")
+
+#def delete_posts(id:int):
